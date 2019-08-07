266e30b96bf50e3d221e123d593f401762a18252
devdiabloh <devdiabloh@gmail.com>
2019-08-07 19:11:15 +0900

    Update DragUI.cs

--- DragUI.cs
+++ DragUI.cs
@@ -14,6 +14,13 @@ public class DragUI : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHa
     private Rect rect;
     private RectTransform rectTransform;
 
+    private float x;
+    private float y;
+    private float minX;
+    private float maxX;
+    private float minY;
+    private float maxY;
+
     public void OnBeginDrag(PointerEventData eventData)
     {
         beginDragObjectPosition = rectTransform.position;
@@ -23,30 +30,65 @@ public class DragUI : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHa
 
     public void OnDrag(PointerEventData eventData)
     {
-        if(rectTransform.anchorMin.x == 1)
-        {
-            m_PosX = Mathf.Clamp(eventData.position.x - m_PositionCorrection.x, rect.width, Screen.width);
-        }
-        else if(rectTransform.anchorMin.x == 0)
-        {
-            m_PosX = Mathf.Clamp(eventData.position.x - m_PositionCorrection.x, 0f, Screen.width - rect.width);
-        }
-        else
-        {
-            m_PosX = Mathf.Clamp(eventData.position.x - m_PositionCorrection.x, (rect.width / 2f), Screen.width - (rect.width/2f));
-        }
+        x = eventData.position.x - m_PositionCorrection.x;
+        y = eventData.position.y - m_PositionCorrection.y;
 
-        if (rectTransform.anchorMin.y == 0)
+        if (AnchorsCheck(0,1,1,1)) // TOP
         {
-            m_PosY = Mathf.Clamp(eventData.position.y - m_PositionCorrection.y, 0, Screen.height - rect.height);
+            m_PosX = Screen.width / 2f;
+            m_PosY = Mathf.Clamp(y, rect.height, Screen.height);
         }
-        else if(rectTransform.anchorMin.y == 1)
+        else if (AnchorsCheck(0,0,1,0)) // BOTTOM
         {
-            m_PosY = Mathf.Clamp(eventData.position.y - m_PositionCorrection.y, rect.height, Screen.height);
+            m_PosX = Screen.width / 2f;
+            m_PosY = Mathf.Clamp(y, 0f, Screen.height - rect.height);
+        }
+        else if(AnchorsCheck(0,0,0,1)) // LEFT
+        {
+            m_PosX = Mathf.Clamp(x, 0f, Screen.width - rect.width);
+            m_PosY = Screen.height / 2f;
+        }
+        else if (AnchorsCheck(1,0,1,1))
+        {
+            m_PosX = Mathf.Clamp(x, rect.width, Screen.width);
+            m_PosY = Screen.height / 2f;
         }
         else
         {
-            m_PosY = Mathf.Clamp(eventData.position.y - m_PositionCorrection.y, (rect.height / 2f), Screen.height - (rect.height/2f));
+            if (rectTransform.anchorMin.x == 1)
+            {
+                minX = rect.width;
+                maxX = Screen.width;
+            }
+            else if (rectTransform.anchorMin.x == 0)
+            {
+                minX = 0f;
+                maxX = Screen.width - rect.width;
+            }
+            else
+            {
+                minX = rect.width / 2f;
+                maxX = Screen.width - minX;
+            }
+
+            if (rectTransform.anchorMin.y == 0)
+            {
+                minY = 0;
+                maxY = Screen.height - rect.height;
+            }
+            else if (rectTransform.anchorMin.y == 1)
+            {
+                minY = rect.height;
+                maxY = Screen.height;
+            }
+            else
+            {
+                minY = rect.height / 2f;
+                maxY = Screen.height - (minY);
+            }
+
+            m_PosX = Mathf.Clamp(x, minX, maxX);
+            m_PosY = Mathf.Clamp(y, minY, maxY);
         }
 
         transform.position = new Vector2(m_PosX, m_PosY);
@@ -61,5 +103,13 @@ public class DragUI : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHa
     {
         rectTransform = this.transform.GetComponent<RectTransform>();
         rect = rectTransform.rect;
+    }
+
+    private bool AnchorsCheck(int minX, int minY, int maxX, int maxY)
+    {
+        return (rectTransform.anchorMin.x == minX &&
+            rectTransform.anchorMin.y == minY &&
+            rectTransform.anchorMax.x == maxX &&
+            rectTransform.anchorMax.y == maxY);
     }
 }
