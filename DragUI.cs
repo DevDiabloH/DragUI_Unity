3257ad1d215eb88595f65ef94215ea6f90bd65cc
DevDiabloH <50089624+DevDiabloH@users.noreply.github.com>
2019-12-06 10:06:41 +0900

    Update DragUI.cs
    update

--- DragUI.cs
+++ DragUI.cs
@@ -1,94 +1,99 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
+[RequireComponent(typeof(RectTransform))]
 public class DragUI : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    private float m_X;
+    private float m_Y;
     private float m_PosX;
     private float m_PosY;
+    private float m_MinX;
+    private float m_MaxX;
+    private float m_MinY;
+    private float m_MaxY;
 
     private Vector3 m_PositionCorrection;
+    private Vector2 m_BeginDragMousePosition;
+    private Vector2 m_BeginDragObjectPosition;
 
-    private Vector2 beginDragMousePosition;
-    private Vector2 beginDragObjectPosition;
+    private Rect m_Rect;
+    private RectTransform m_RectTransform;
 
-    private Rect rect;
-    private RectTransform rectTransform;
-
-    private float x;
-    private float y;
-    private float minX;
-    private float maxX;
-    private float minY;
-    private float maxY;
+    private void Awake()
+    {
+        m_RectTransform = this.transform.GetComponent<RectTransform>();
+        m_Rect = m_RectTransform.rect;
+    }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        beginDragObjectPosition = rectTransform.position;
-        beginDragMousePosition = eventData.pressPosition;
-        m_PositionCorrection = beginDragMousePosition - beginDragObjectPosition;
+        m_BeginDragObjectPosition = m_RectTransform.position;
+        m_BeginDragMousePosition = eventData.pressPosition;
+        m_PositionCorrection = m_BeginDragMousePosition - m_BeginDragObjectPosition;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        x = eventData.position.x - m_PositionCorrection.x;
-        y = eventData.position.y - m_PositionCorrection.y;
+        m_X = eventData.position.x - m_PositionCorrection.x;
+        m_Y = eventData.position.y - m_PositionCorrection.y;
 
-        if (AnchorsCheck(0,1,1,1)) // TOP
+        if (AnchorsCheck(0, 1, 1, 1)) // TOP
         {
             m_PosX = Screen.width / 2f;
-            m_PosY = Mathf.Clamp(y, rect.height, Screen.height);
+            m_PosY = Mathf.Clamp(m_Y, m_Rect.height, Screen.height);
         }
-        else if (AnchorsCheck(0,0,1,0)) // BOTTOM
+        else if (AnchorsCheck(0, 0, 1, 0)) // BOTTOM
         {
             m_PosX = Screen.width / 2f;
-            m_PosY = Mathf.Clamp(y, 0f, Screen.height - rect.height);
+            m_PosY = Mathf.Clamp(m_Y, 0f, Screen.height - m_Rect.height);
         }
-        else if(AnchorsCheck(0,0,0,1)) // LEFT
+        else if (AnchorsCheck(0, 0, 0, 1)) // LEFT
         {
-            m_PosX = Mathf.Clamp(x, 0f, Screen.width - rect.width);
+            m_PosX = Mathf.Clamp(m_X, 0f, Screen.width - m_Rect.width);
             m_PosY = Screen.height / 2f;
         }
-        else if (AnchorsCheck(1,0,1,1))
+        else if (AnchorsCheck(1, 0, 1, 1)) // RIGHT
         {
-            m_PosX = Mathf.Clamp(x, rect.width, Screen.width);
+            m_PosX = Mathf.Clamp(m_X, m_Rect.width, Screen.width);
             m_PosY = Screen.height / 2f;
         }
-        else
+        else // CENTER
         {
-            if (rectTransform.anchorMin.x == 1)
+            if (m_RectTransform.anchorMin.x == 1)
             {
-                minX = rect.width;
-                maxX = Screen.width;
+                m_MinX = m_Rect.width;
+                m_MaxX = Screen.width;
             }
-            else if (rectTransform.anchorMin.x == 0)
+            else if (m_RectTransform.anchorMin.x == 0)
             {
-                minX = 0f;
-                maxX = Screen.width - rect.width;
+                m_MinX = 0f;
+                m_MaxX = Screen.width - m_Rect.width;
             }
             else
             {
-                minX = rect.width / 2f;
-                maxX = Screen.width - minX;
+                m_MinX = m_Rect.width / 2f;
+                m_MaxX = Screen.width - m_MinX;
             }
 
-            if (rectTransform.anchorMin.y == 0)
+            if (m_RectTransform.anchorMin.y == 0)
             {
-                minY = 0;
-                maxY = Screen.height - rect.height;
+                m_MinY = 0;
+                m_MaxY = Screen.height - m_Rect.height;
             }
-            else if (rectTransform.anchorMin.y == 1)
+            else if (m_RectTransform.anchorMin.y == 1)
             {
-                minY = rect.height;
-                maxY = Screen.height;
+                m_MinY = m_Rect.height;
+                m_MaxY = Screen.height;
             }
             else
             {
-                minY = rect.height / 2f;
-                maxY = Screen.height - (minY);
+                m_MinY = m_Rect.height / 2f;
+                m_MaxY = Screen.height - (m_MinY);
             }
 
-            m_PosX = Mathf.Clamp(x, minX, maxX);
-            m_PosY = Mathf.Clamp(y, minY, maxY);
+            m_PosX = Mathf.Clamp(m_X, m_MinX, m_MaxX);
+            m_PosY = Mathf.Clamp(m_Y, m_MinY, m_MaxY);
         }
 
         transform.position = new Vector2(m_PosX, m_PosY);
@@ -99,17 +104,11 @@ public class DragUI : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHa
 
     }
 
-    private void Awake()
-    {
-        rectTransform = this.transform.GetComponent<RectTransform>();
-        rect = rectTransform.rect;
-    }
-
     private bool AnchorsCheck(int minX, int minY, int maxX, int maxY)
     {
-        return (rectTransform.anchorMin.x == minX &&
-            rectTransform.anchorMin.y == minY &&
-            rectTransform.anchorMax.x == maxX &&
-            rectTransform.anchorMax.y == maxY);
+        return (m_RectTransform.anchorMin.x == minX &&
+            m_RectTransform.anchorMin.y == minY &&
+            m_RectTransform.anchorMax.x == maxX &&
+            m_RectTransform.anchorMax.y == maxY);
     }
 }
